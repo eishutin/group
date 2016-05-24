816c16383aa7e5579ab3e4e48f33e809b389b2f8
eishutin <evg.ishutin@gmail.com>
2016-05-24 20:07:18 +0300

    a little changes

--- Program1.pas
+++ Program1.pas
@@ -1,7 +1,7 @@
 begin
- writeln('Наша группа:');
- writeln('1.Алиса');
- writeln('2.Женя');
- writeln('3.Ваня');
- writeln('4.Даня');
+ writeln('Our group:');
+ writeln('1.Alisa');
+ writeln('2.Zhenia');
+ writeln('3.Vanya');
+ writeln('4.Danya');
 end.
